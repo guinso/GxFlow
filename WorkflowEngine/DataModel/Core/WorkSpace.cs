de75e0b04246a82e60e9a97ff0ad35bd2756e933
guinso <chet_siang@yahoo.com>
2025-10-24 18:19:21 +0800

    implement basic code

--- WorkflowEngine/DataModel/Core/WorkSpace.cs
+++ WorkflowEngine/DataModel/Core/WorkSpace.cs
@@ -1,7 +1,70 @@
-﻿namespace GxFlow.WorkflowEngine.DataModel.Core
+﻿using GxFlow.WorkflowEngine.DataModel.Xml;
+using System.Xml.Serialization;
+
+namespace GxFlow.WorkflowEngine.DataModel.Core
 {
-    public interface IWorkspace: IGraphObj
+    public interface IWorkSpace: IGraphObj
     {
-        IEnumerable<IDiagram> Diagrams { get; }
+        List<IDiagramExt> Diagrams { get; }
+    }
+
+    public interface IWorkSpaceExt: IWorkSpace, IScriptTransformer
+    {
+
+    }
+
+    public abstract class WorkSpaceBase : IWorkSpaceExt
+    {
+        protected string _type = string.Empty;
+
+        [XmlAttribute("id")]
+        public string ID { get; set; } = Guid.NewGuid().ToString("N");
+
+        [XmlAttribute("type")]
+        public string TypeName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_type))
+                    _type = GetType().FullName;
+
+                return _type;
+            }
+
+            set
+            {
+                _type = value;
+            }
+        }
+
+        [XmlAttribute("name")]
+        public string DisplayName { get; set; } = string.Empty;
+
+        [XmlAttribute("note")]
+        public string Note { get; set; } = string.Empty;
+
+        [XmlIgnore]
+        public List<IDiagramExt> Diagrams { get; protected set; } = new List<IDiagramExt>();
+
+        [XmlElement("diagrams")]
+        public XmlHolderArr<IDiagramExt> XmlDiagrams { get; set; } = new XmlHolderArr<IDiagramExt>();
+
+        [XmlAttribute("defaultdiagram")]
+        public string DefaultDiagramID { get; set; } = string.Empty;
+
+        public Task GetTaskStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Run(GraphVariable vars, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToCSharp(GraphVariable vars)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
